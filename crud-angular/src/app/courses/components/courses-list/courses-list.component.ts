9fe6432b2d249c5cc20baf0bacc6cf221039a847
Luis Fernando <luis.silva9902@gmail.com>
2023-04-14 08:46:31 -0300

    Created new components, routing to edit course and Guards routes

--- crud-angular/src/app/courses/components/courses-list/courses-list.component.ts
+++ crud-angular/src/app/courses/components/courses-list/courses-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Course } from '../model/course';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-courses-list',
@@ -10,14 +9,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CoursesListComponent {
 
   @Input() courses: Course[] = []
+  @Output() add = new EventEmitter(false)
+  @Output() edit = new EventEmitter(false)
   readonly displayedColumns = ['name', 'category', 'actions']
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
-
   onAdd(){
-    this.router.navigate(['new'], {relativeTo: this.route})
+    this.add.emit(true)
+  }
+
+  onEdit(course: Course){
+    this.edit.emit(course)
   }
 }
--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
-import { CoursesService } from '../services/courses.service';
+import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
   
   form = this.formBuilder.group({
+    _id: [''],
     name: [''],
     category: [''],
   });
@@ -20,8 +23,18 @@ export class CourseFormComponent {
     private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private location: Location
+    private location: Location,
+    private activedRoute: ActivatedRoute
   ) {}
+
+  ngOnInit(): void {
+      const course: Course = this.activedRoute.snapshot.data['course']
+      this.form.setValue({
+        _id: course._id,
+        name: course.name,
+        category: course.category
+      })
+  }
 
   onSubmit(){
     this.service.save(this.form.value).subscribe({
