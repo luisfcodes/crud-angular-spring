d40788e7b9cad43af778525150c3c9724535bf31
Luis Fernando <luis.silva9902@gmail.com>
2023-04-14 14:37:01 -0300

    Created back-end validation

--- crud-spring/src/main/java/com/luis/model/Course.java
+++ crud-spring/src/main/java/com/luis/model/Course.java
@@ -1,5 +1,9 @@
 package com.luis.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,10 +11,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?")
+@Where(clause = "status = 'Active'")
 public class Course {
   
   @Id
@@ -18,10 +27,21 @@ public class Course {
   @JsonProperty("_id")
   private Long id;
 
+  @NotBlank
+  @NotNull
+  @Length(min = 5, max = 200)
   @Column(length = 200, nullable = false)
   private String name;
 
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Back-end|Front-end")
   @Column(length = 10, nullable = false)
   private String category;
 
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Active|Inactive")
+  @Column(length = 10, nullable = false)
+  private String status = "Active";
 }
